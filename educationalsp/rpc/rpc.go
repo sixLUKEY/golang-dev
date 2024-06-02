d82dd11a95e847ef698daca6c15c435a23e6f1a8
sixLUKEY <marcoslukerojo@gmail.com>
2024-06-02 21:15:12 +0200

    DecodeMessage progress

---
+++ educationalsp/rpc/rpc.go
@@ -0,0 +1,38 @@
+package rpc
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"strconv"
+)
+
+func EncodeMessage(msg any) string {
+	content, err := json.Marshal(msg)
+
+	if err != nil {
+		panic(err)
+	}
+
+	return fmt.Sprintf("Content-Length: %d\r\n\r\n%s", len(content), content)
+}
+
+func DecodeMessage(msg []byte) (int, error) {
+	header, content, found := bytes.Cut(msg, []byte{'\r', '\n', '\r', '\n'})
+	if !found {
+		return 0, errors.New("Did not find separator")
+	}
+
+	// Content-Length: <number>
+	contentLengthBytes := header[len("Content-Length: "):]
+	contentLength, err := strconv.Atoi(string(contentLengthBytes))
+	if err != nil {
+		return 0, err
+	}
+
+	// TODO: will get to
+	_ = content
+
+	return contentLength, nil
+}
