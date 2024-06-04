500738521a7858484870c59c6b8e722194470bc0
sixLUKEY <marcoslukerojo@gmail.com>
2024-06-04 21:20:21 +0200

    cryptopals challenge 1 and tests complete

---
+++ cryptopals/set_1/challenge_1.go
@@ -0,0 +1,16 @@
+package hexbase64
+
+import (
+	"encoding/base64"
+	"encoding/hex"
+)
+
+func HexToBase64(hexString string) (string, error) {
+	bytes, err := hex.DecodeString(hexString)
+	if err != nil {
+		return "", err
+	}
+
+	base64String := base64.RawStdEncoding.EncodeToString(bytes)
+	return base64String, nil
+}
---
+++ cryptopals/set_1/challenge_1_test.go
@@ -0,0 +1,23 @@
+package hexbase64
+
+import (
+	"testing"
+)
+
+func TextHexToBase(t *testing.T) {
+	hextString := "48656c6c6f20576f726c64"
+	expectedBase64 := "SGVsbG8gV29ybGQ="
+	actualBase64, err := HexToBase64(hextString)
+	if err != nil {
+		t.Errorf("Error converting hex to base64: %v", err)
+	}
+	if actualBase64 != expectedBase64 {
+		t.Errorf("Expected %s, but got %s", expectedBase64, actualBase64)
+	}
+
+	invalidHexString := "invalidHex"
+	_, err = HexToBase64(invalidHexString)
+	if err == nil {
+		t.Errorf("Expected an error for invalid hex string, but got nil")
+	}
+}
