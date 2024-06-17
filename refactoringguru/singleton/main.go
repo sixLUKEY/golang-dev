3066d94b2848e81e6273f268344b2b40c7298ce7
sixLUKEY <marcoslukerojo@gmail.com>
2024-06-17 10:00:05 +0200

    singleton pattern completed

---
+++ refactoringguru/singleton/main.go
@@ -0,0 +1,15 @@
+// Client code
+
+package main
+
+import "fmt"
+
+func main() {
+	for i := 0; i < 30; i++ {
+		go getInstance()
+	}
+
+	// Scanln is similar to Scan, but tops scanning at a newline and
+	// after the final item there must be a newline or EOF.
+	fmt.Scanln()
+}
