ac71374193c4e230d0e6997b4b286ea98541937f
sixLUKEY <marcoslukerojo@gmail.com>
2024-06-17 11:17:30 +0200

    bridge pattern completed

---
+++ refactoringguru/structural/bridge/windows.go
@@ -0,0 +1,18 @@
+// Refined abstraction
+
+package main
+
+import "fmt"
+
+type Windows struct {
+	printer Printer
+}
+
+func (w *Windows) Print() {
+	fmt.Println("Print request for Windows.")
+	w.printer.PrintFile()
+}
+
+func (w *Windows) SetPrinter(p Printer) {
+	w.printer = p
+}
