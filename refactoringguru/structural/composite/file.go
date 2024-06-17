a62980dddf2f50d797d2523bd75a04bead144777
sixLUKEY <marcoslukerojo@gmail.com>
2024-06-17 13:57:52 +0200

    composite pattern completed

---
+++ refactoringguru/structural/composite/file.go
@@ -0,0 +1,17 @@
+// Leaf
+
+package main
+
+import "fmt"
+
+type File struct {
+	name string
+}
+
+func (f *File) search(keyword string) {
+	fmt.Printf("Searching for keyword %s in file %s\n", keyword, f.name)
+}
+
+func (f *File) getName() string {
+	return f.name
+}
